00059429353e3b54582a9478a6c0e11bd99a9f7b
golfstrimmar <64115098+golfstrimmar@users.noreply.github.com>
2022-05-08 22:43:49 +0200

    777

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -1,72 +1,65 @@
-'use ctrict'
+"use ctrict";
 
-const MyTabs = () => {
-    let tabs = document.querySelectorAll('.tabs-container-js');
-    tabs.forEach((cell)=>{
-        let singles = Array.prototype.slice.call(cell.children);
+const MyTabs = (tab) => {
+  let title = tab.querySelector(".tab-title-js");
+  let span = title.querySelector("span");
+  let hidden = tab.querySelector(".tab-hidden-js");
+  let titleSvg = title.querySelector("svg");
+  hidden.style.height = "0px";
 
-        singles.forEach((c) => {
-            let title = c.querySelector('.tab-title-js')
-            let span = title.querySelectorAll(' span');
-            let hidden = c.querySelector('.tab-hidden-js');
-            let svg = c.querySelector('.tab-title-js i');
-            hidden.style.height = "0px"
-           
-            function openItems(e) {
-
-  if (e.target !== c) {
-   title.classList.add("is-active");
-   hidden.classList.add("is-active");
-   svg.classList.add("is-active");
-   hidden.style.height = `${hidden.scrollHeight}px`;
-   hidden.animate([{ height: "0px" }, { height: `${hidden.scrollHeight}px` }], {
-     duration: 300,
-     easing: "ease-in-out",
-   });
-  }
-
-
-  if (!hidden.classList.contains("is-active")) {
+  const openHidden = () => {
     title.classList.add("is-active");
     hidden.classList.add("is-active");
-    svg.classList.add("is-active");
+    titleSvg.classList.add("is-active");
     hidden.style.height = `${hidden.scrollHeight}px`;
     hidden.animate(
       [{ height: "0px" }, { height: `${hidden.scrollHeight}px` }],
       {
-        duration: 300,
+        duration: 200,
         easing: "ease-in-out",
       }
     );
-  } else {
+  };
+
+  const closeHidden = () => {
     title.classList.remove("is-active");
     hidden.classList.remove("is-active");
-    svg.classList.remove("is-active");
+    titleSvg.classList.remove("is-active");
     hidden.animate(
       [{ height: `${hidden.scrollHeight}px` }, { height: "0px" }],
       {
-        duration: 300,
+        duration: 200,
         easing: "ease-in-out",
       }
     );
     hidden.style.height = "0px";
-  }
-            }
+  };
 
-
-            title.addEventListener('click', openItems)
-
-        })
-    })
-
-};
-document.addEventListener('DOMContentLoaded', function(){
-    const tabs = document.querySelectorAll('.tabs-container-js ');
-    if(tabs.length> 0){
-        MyTabs();
+  function openItems(e) {
+    if (
+      (e.target == title || e.target == titleSvg || e.target == span) &&
+      !hidden.classList.contains("is-active")
+    ) {
+      openHidden();
+    } else if (e.target == title && hidden.classList.contains("is-active")) {
+      closeHidden();
+    } else if (
+      hidden.classList.contains("is-active") &&
+      !e.target.closest(".tab-hidden-js")
+    ) {
+      closeHidden();
     }
+  }
 
+  document.addEventListener("click", openItems);
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  const tabs = document.querySelectorAll(".tab");
+
+  if (tabs.length > 0) {
+    tabs.forEach((tab) => {
+      MyTabs(tab);
+    });
+  }
 });
-
-
-
